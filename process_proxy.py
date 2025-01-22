77ebfc0d83cddb46036721581d48b7a667e4c10d
kcepu877 <kcepu877@gmail.com>
2025-01-23 00:01:46 +0700

    Update process_proxy.py

--- process_proxy.py
+++ process_proxy.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 # Nama file proxy yang diunggah
 file_name = "proxy_ip_port.txt"  # Nama file Anda
@@ -17,11 +18,26 @@ with open(file_name, 'r') as file:
 # Membuat DataFrame dengan data yang valid
 df = pd.DataFrame(data, columns=['ip', 'port'])
 
+# Fungsi untuk mendapatkan id (kode negara) dan ISP berdasarkan IP
+def get_ip_info(ip):
+    try:
+        # Menggunakan API ipinfo.io untuk mendapatkan informasi IP
+        response = requests.get(f"https://ipinfo.io/{ip}/json")
+        data = response.json()
+        country = data.get('country', 'Unknown')  # Kode negara
+        isp = data.get('org', 'Unknown')         # ISP
+        return country, isp
+    except Exception as e:
+        return 'Unknown', 'Unknown'
+
+# Menambahkan kolom 'id' dan 'isp' berdasarkan IP
+df[['id', 'isp']] = df['ip'].apply(lambda x: pd.Series(get_ip_info(x)))
+
 # Menyaring duplikat berdasarkan kolom 'ip' dan 'port'
 df_unique = df.drop_duplicates(subset=['ip', 'port'], keep='first')
 
 # Menyimpan hasil ke file TXT
-output_file = 'proxy_ip_port_cleaned.txt'
+output_file = 'proxy_ip_port_with_id_isp.txt'
 df_unique.to_csv(output_file, index=False, sep=',', header=False)
 
 # Menyediakan file untuk diunduh
